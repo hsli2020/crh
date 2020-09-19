5195470e567fa892a593cfb845d726092c038b1c
Hongshuan L <lihsca@gmail.com>
2020-09-19 12:13:01 -0400

    generateBaseline

--- app/services/DataService.php
+++ app/services/DataService.php
@@ -145,4 +145,109 @@ class DataService extends Injectable
 
         return $result;
     }
+
+    public function generateBaseline()
+    {
+        $b1 = $this->calcBaseline(51); // Meter-1
+        $b2 = $this->calcBaseline(52); // Meter-2
+
+        $this->db->execute('TRUNCATE TABLE crh_baseline');
+
+        foreach (range(0, 23) as $hour) {
+            $this->db->insertAsDict('crh_baseline', [
+                'hour'   => $hour,
+                'meter1' => $b1[$hour],
+                'meter2' => $b2[$hour],
+            ]);
+        }
+    }
+
+    protected function calcBaseline($meter)
+    {
+        $date = date('Y-m-d');
+
+        $start = date('Y-m-d', strtotime('-35 day'));
+        $sql = "SELECT time AS time_utc,
+                       CONVERT_TZ(time, 'UTC', 'America/Toronto') AS time_edt,
+                       CONVERT_TZ(time, 'UTC', 'EST') AS time_est,
+                       kva AS kw
+                  FROM p{$meter}_mb_001_genmeter
+                HAVING time_edt>='$start' AND time_edt<'$date'
+              ORDER BY time DESC";
+        $data = $this->db->fetchAll($sql);
+
+        $season = getSeason($date);
+
+        $daily = [];
+        foreach ($data as $rec) {
+            $time = $rec['time_edt'];
+            $kwh = $rec['kw'];
+
+            $dt = substr($time, 0, 10);
+            $hr = substr($time, 11, 2);
+
+            if (isWeekend($dt) || isHoliday($dt) || isMaintenance($dt)) {
+                continue;
+            }
+
+            if (getSeason($dt) != $season) {
+                break; // shouldn't cross seasons (SUMMER/WINTER)
+            }
+
+            if (isset($daily[$dt])) {
+                $daily[$dt]['total'] += $kwh;
+            } else {
+                $daily[$dt]['total'] = $kwh;
+            }
+
+            if (isset($daily[$dt]['hourly'][$hr])) {
+                $daily[$dt]['hourly'][$hr]['sum'] += $kwh;
+                $daily[$dt]['hourly'][$hr]['cnt'] += 1;
+            } else {
+                $daily[$dt]['hourly'][$hr]['sum'] = $kwh;
+                $daily[$dt]['hourly'][$hr]['cnt'] = 1;
+            }
+
+            if (count($daily) == 20+1) {
+                array_pop($daily);
+                break;
+            }
+        }
+
+        uasort($daily, function($a, $b) {
+            if ($a['total'] == $b['total']) { return 0; }
+            return ($a['total'] < $b['total']) ? 1 : -1;
+        });
+
+        $top15 = array_slice($daily, 0, 15);
+
+        $hourly = [];
+        foreach ($top15 as $day) {
+            foreach ($day['hourly'] as $hour => $rec) {
+                if (isset($hourly[$hour])) {
+                    $hourly[$hour]['sum'] += $rec['sum'];
+                    $hourly[$hour]['cnt'] += $rec['cnt'];
+                } else {
+                    $hourly[$hour]['sum'] = $rec['sum'];
+                    $hourly[$hour]['cnt'] = 1;
+                }
+            }
+        }
+
+        foreach ($hourly as $hour => $rec) {
+            $avg = round($rec['sum']/$rec['cnt']);
+            $hourly[$hour]['avg'] = $avg;
+        }
+
+        // Baseline (Avg)
+        $result = [];
+        foreach ($hourly as $hour => $rec) {
+            $h = intval($hour); // chart requires number, not string
+            $avg = $rec['avg'];
+            $result[$h] = $avg;
+        }
+
+        ksort($result); // sort by hour
+        return $result;
+    }
 }
