d12614d0d30e4dd98b0d693bf402794c3bf7dc67
Hongshuan L <lihsca@gmail.com>
2020-09-20 14:49:28 -0400

    data import

--- app/services/ImportService.php
+++ app/services/ImportService.php
@@ -8,17 +8,28 @@ class ImportService extends Injectable
 {
     public function import()
     {
-        $this->log('Start importing');
+        $this->log('Start importing CRH Data');
 
-        $projects = $this->projectService->getAll();
+        $meters = [
+            [
+                'name'   => 'Meter 1',
+                'ftpdir' => 'CRH1__001EC6056350',
+                'table'  => 'crh_meter_1',
+            ],
+            [
+                'name'   => 'Meter 2',
+                'ftpdir' => 'CRH2_001EC6055C22',
+                'table'  => 'crh_meter_2',
+            ],
+        ];
 
         $ftpRoot = "C:\\GCS-FTP-ROOT\\";
 
         $fileCount = 0;
-        foreach ($projects as $project) {
-            echo $project->name, EOL;
+        foreach ($meters as $meter) {
+            echo $meter['name'], EOL;
 
-            $dir = $ftpRoot . $project->ftpdir;
+            $dir = $ftpRoot . $meter['ftpdir'];
             foreach (glob($dir . '/*.csv') as $filename) {
                 echo "\t", $filename, EOL;
 
@@ -29,16 +40,50 @@ class ImportService extends Injectable
 
                 $fileCount++;
 
-                $this->importFile($filename, $project);
+                $this->importFile($filename, $meter);
                 $this->backupFile($filename, $dir);
-            }
-
-            if ($project->cbdir) {
-                $this->importCombiners($project, $ftpRoot . $project->cbdir);
             }
         }
 
         $this->log("Importing completed, $fileCount file(s) imported.\n");
+    }
+
+    protected function importFile($filename, $meter)
+    {
+        // time(UTC),error,lowalarm,highalarm,"KW_total (kW)","kwh_del (kWh)","kwh_rec (kWh)","vln_a (Volts)","vln_b (Volts)","vln_c (Volts)"
+        // filename: c:\FTP-Backup\125Bermondsey_001EC6053434\mb-001.57BEE4B7_1.log.csv
+
+        $table = $meter['table'];
+        $columns = [
+            'time',
+            'error',
+            'low_alarm',
+            'high_alarm',
+            'kva', // kw
+            'kwh_del',
+            'kwh_rec',
+            'vln_a',
+            'vln_b',
+            'vln_c',
+        ];
+
+        if (($handle = fopen($filename, "r")) !== FALSE) {
+            fgetcsv($handle); // skip first line
+            while (($fields = fgetcsv($handle)) !== FALSE) {
+                if (count($columns) != count($fields)) {
+                    $this->log("DATA ERROR: $filename\n\t" . implode(', ', $fields));
+                    continue;
+                };
+
+                $data = array_combine($columns, $fields);
+                try {
+                    $this->db->insertAsDict($table, $data);
+                } catch (\Exception $e) {
+                    echo $e->getMessage(), EOL;
+                }
+            }
+            fclose($handle);
+        }
     }
 
     protected function backupFile($filename, $ftpdir)
@@ -52,44 +97,6 @@ class ImportService extends Injectable
         $newfile = $dir . '\\' . basename($filename);
         if (filesize($filename) > 0) {
             rename($filename, $newfile);
-        }
-    }
-
-    protected function importFile($filename, $project)
-    {
-        // filename: c:\FTP-Backup\125Bermondsey_001EC6053434\mb-001.57BEE4B7_1.log.csv
-        $parts = explode('.', basename($filename));
-        $dev  = $parts[0]; // mb-001
-        $hash = $parts[1]; // 57BEE4B7_1
-
-        if (!isset($project->devices[$dev])) {
-           #$this->log("Invalid Filename: $filename");
-            return;
-        }
-
-        $device  = $project->devices[$dev];
-        $columns = $device->getTableColumns();
-
-        if (($handle = fopen($filename, "r")) !== FALSE) {
-            $latest = [];
-
-            fgetcsv($handle); // skip first line
-            while (($fields = fgetcsv($handle)) !== FALSE) {
-                if (count($columns) != count($fields)) {
-                    $this->log("DATA ERROR: $filename\n\t" . implode(', ', $fields));
-                    continue;
-                };
-
-                $data = array_combine($columns, $fields);
-                $data = $this->fixValues($project, $dev, $data);
-
-                $this->insertIntoDeviceTable($project, $device, $data);
-
-                $latest = $data;
-            }
-            fclose($handle);
-
-            $this->saveLatestData($project, $device, $latest);
         }
     }
 
