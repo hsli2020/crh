83024e43d2b7bc9c8b4003ce0e3bef998536d702
Hongshuan L <lihsca@gmail.com>
2020-11-01 18:59:34 -0500

    export baseline+actualload

--- app/services/ExportService.php
+++ app/services/ExportService.php
@@ -65,19 +65,34 @@ EOS;
         $filename = "$basedir./tmp/crh-baseline-$now.csv";
 
         $fp = fopen($filename, 'w');
-        fputcsv($fp, [ "time(EST)", "Meter1", "Meter2" ]);
+        fputcsv($fp, [ "time(EST)", "Baseline", "Actual-Load" ]);
 
-        $sql = "SELECT * FROM crh_baseline_history WHERE date>='$startTime' AND date<='$endTime'";
+        $sql = "SELECT b.date,
+                       b.meter1 AS m1b,
+                       b.meter2 AS m2b,
+                       a.meter1 AS m1a,
+                       a.meter2 AS m2a
+                  FROM crh_baseline_history  b
+                  JOIN crh_actual_load       a ON a.date=b.date
+                 WHERE b.date>='$startTime' AND b.date<='$endTime'";
         $rows = $this->db->fetchAll($sql);
+
         foreach ($rows as $row) {
             $date = $row['date'];
-            $meter1 = json_decode($row['meter1'], 1);
-            $meter2 = json_decode($row['meter2'], 1);
-            foreach ($meter1 as $hour => $m1kw) {
-                if ($hour >= 8 && $hour <= 22) {
-                    $m2kw = $meter2[$hour];
-                    fputcsv($fp, [ "$date $hour:00", $m1kw, $m2kw ]);
-                }
+            $m1b = json_decode($row['m1b'], 1);
+            $m2b = json_decode($row['m2b'], 1);
+
+            $m1a = json_decode($row['m1a'], 1);
+            $m2a = json_decode($row['m2a'], 1);
+
+            foreach (range(8, 22) as $hour) {
+                $m1bkw = $m1b[$hour];
+                $m2bkw = $m2b[$hour];
+
+                $m1akw = $m1a[$hour];
+                $m2akw = $m2a[$hour];
+
+                fputcsv($fp, [ "$date $hour:00", $m1bkw+$m2bkw, $m1akw+$m2akw ]);
             }
         }
 
